837d802e446ef296ad80360dc3bc63c7fedd852d
DeolaDev <deoladeveloper@gmail.com>
2023-01-15 04:33:14 -0500

    new commit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Amplify } from "aws-amplify";
-import awsmobile from "./aws-exports";
+// import awsmobile from "./aws-exports"; - I added this
 
-//import awsExports from "./aws-exports";
+import awsExports from "./aws-exports";
 
-//Amplify.configure(awsExports);
-Amplify.configure(awsmobile);
+Amplify.configure(awsExports);
+//Amplify.configure(awsmobile); - and this
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
