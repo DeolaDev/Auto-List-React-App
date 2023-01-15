cbcb7af99deca2c5509f9e5c5f77d6d530d13e0c
DeolaDev <deoladeveloper@gmail.com>
2023-01-15 04:20:27 -0500

    new commit

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Amplify } from "aws-amplify";
-import awsmobile from "aws-exports";
+import awsmobile from ".aws-exports";
 
-//import awsExports from "./aws-exports";
+import awsExports from "./aws-exports";
 
 Amplify.configure(awsExports);
 
