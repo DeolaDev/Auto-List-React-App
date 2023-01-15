a86fa91594789a591e2368a0617126b95d31666e
DeolaDev <deoladeveloper@gmail.com>
2023-01-15 04:12:10 -0500

    new commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { Amplify } from "aws-amplify";
-import awsExports from "./aws-exports";
+import awsmobile from "aws-exports";
+
+//import awsExports from "./aws-exports";
 
 Amplify.configure(awsExports);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
